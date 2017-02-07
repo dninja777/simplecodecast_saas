12ee1452f4ce3092001bfe388bf482f505964379
Duron Epps <duroneppsjr@hotmail.com>
2017-02-07 03:02:33 +0000

    added validations and create action

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ActiveRecord::Base
    
-    
+   #validates that form must have been field out 
+   validates :name, presence: true
+   validates :email, presence: true
+   validates :comments, presence: true
     
 end
